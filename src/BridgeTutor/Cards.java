aed6e3b3e3ed34b100256cd89adad5c85206dc62
GAM <GAM@196.42.110.138>
2019-08-07 19:20:02 +0200

    HORRENDOUS CODE FOR TESTING

--- src/BridgeTutor/Cards.java
+++ src/BridgeTutor/Cards.java
@@ -10,10 +10,59 @@ package BridgeTutor;
  * @author GAM
  */
 public class Cards {
-    //enum private Suit suit;
-    //enum private CardValue cardValue;
+    private Suit suit;
+    private CardValue cardValue;
+    
+    
+    public enum Suit {
+        SPADES,
+        HEARTS,
+        DIAMONDS,
+        CLUBS;
+    }
+    
+    public enum CardValue {
+        TWO(2),
+        THREE(3),
+        FOUR(4),
+        FIVE(5),
+        SIX(6),
+        SEVEN(7),
+        EIGHT(8),
+        NINE(9),
+        TEN(10),
+        JACK(11),
+        QUEEN(12),
+        KING(13),
+        ACE(14);
+        
+        
+        private int cardValue;
+        
+        private CardValue(int value) {
+            this.cardValue = value;
+        }
+        
+        public int getValue() {
+            return cardValue;
+        }
+        
+        }
     
     public Cards(){
-        
+
+    }
+    
+    public Cards(CardValue cardValue, Suit suit) {
+        this.cardValue = cardValue;
+        this.suit = suit;
+    }
+    
+    public Suit getSuit(){
+        return suit;
+    }
+    
+    public CardValue getValue(){
+        return cardValue;
     }
 }
--- src/BridgeTutor/Deck.java
+++ src/BridgeTutor/Deck.java
@@ -10,11 +10,34 @@ package BridgeTutor;
  * @author GAM
  */
 public class Deck {
-    private Cards[] cards;
-    
+    private Cards[] cards = new Cards[52];
+    private Cards pop;
+    private Cards.CardValue[] values = new Cards.CardValue[13];
+    private Cards.Suit[] suits = new Cards.Suit[4];
     public Deck(){
-        
+         
     }
+    public void populate() {
+        values[0] = Cards.CardValue.TWO; //= [Cards.CardValue.TWO, Cards.CardValue.THREE, Cards.CardValue.THREE, Cards.CardValue.THREE, Cards.CardValue.THREE, Cards.CardValue.THREE, Cards.CardValue.THREE, Cards.CardValue.THREE, Cards.CardValue.THREE, Cards.CardValue.THREE, Cards.CardValue.THREE, Cards.CardValue.THREE,Cards.CardValue.THREE];
+        values[1] = Cards.CardValue.THREE;
+        values[2] = Cards.CardValue.FOUR;
+        values[3] = Cards.CardValue.FIVE;
+        values[4] = Cards.CardValue.SIX;
+        values[5] = Cards.CardValue.SEVEN;
+        values[6] = Cards.CardValue.EIGHT;
+        values[7] = Cards.CardValue.NINE;
+        values[8] = Cards.CardValue.TEN;
+        values[9] = Cards.CardValue.JACK;
+        values[10] = Cards.CardValue.QUEEN;
+        values[11] = Cards.CardValue.KING;
+        values[12] = Cards.CardValue.ACE;
+        
+        suits[0] = Cards.Suit.SPADES;
+        suits[1] = Cards.Suit.HEARTS;
+        suits[2] = Cards.Suit.DIAMONDS;
+        suits[3] = Cards.Suit.CLUBS;
+    }   
+        
     public Cards[] shuffle() {
         // make random order of cards to shuffle
     return cards;
